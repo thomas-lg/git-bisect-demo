bbfe30ca0b0979ed3e818570f2ed481334566ae9
Thomas Le Gougaud <tom.legougaud@gmail.com>
2019-06-26 11:19:27 +1200

    transform class into function

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-export class App extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1>React Webpack Starterkit</h1>
-			</div>
-		);
-	}
-}
+export const App = () => {
+	return (
+		<div>
+			<h1>React Webpack Starterkit</h1>
+		</div>
+	);
+};
