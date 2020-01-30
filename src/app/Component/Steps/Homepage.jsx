9f3b12f4c803a4d783253b0d9c1d0747b6aa40b5
Le Gougaud Thomas <tom.legougaud@gmail.com>
2020-01-30 19:33:45 +0100

    homepage update

--- src/app/Component/Steps/Homepage.jsx
+++ src/app/Component/Steps/Homepage.jsx
@@ -1,6 +1,36 @@
-import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import React from 'react';
+
+const useStyles = makeStyles({
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		flex: 1,
+		justifyContent: 'space-between'
+	},
+	center: {
+		alignItems: 'center'
+	},
+	powered: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+});
 
 export const Homepage = () => {
-	return <Typography variant='h1'>Demo project for git-bisect</Typography>;
+	const classes = useStyles();
+
+	return (
+		<div className={classes.container + ' ' + classes.center}>
+			<Typography variant='h1'>Demo project for git-bisect</Typography>
+			<Typography variant='h3' component='h3'>
+				<a href='https://github.com/thomas-lg/git-bisect-demo'>
+					https://github.com/thomas-lg/git-bisect-demo
+				</a>
+			</Typography>
+		</div>
+	);
 };
