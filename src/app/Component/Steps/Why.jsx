7f42974f419303a59fd4770acd9d4f29401ab681
Le Gougaud Thomas <tom.legougaud@gmail.com>
2020-01-30 19:42:22 +0100

    working on why slide

--- src/app/Component/Steps/Why.jsx
+++ src/app/Component/Steps/Why.jsx
@@ -1,6 +1,66 @@
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		width: '100%',
+		display: 'flex',
+		flexDirection: 'column',
+		flexGrow: 1,
+		justifyContent: 'space-between',
+		backgroundColor: theme.palette.background.paper
+	},
+	listItemTextLeft: {
+		fontSize: '5em',
+		color: '#4CAF50',
+		display: 'flex',
+		justifyContent: 'flex-start'
+	},
+	listItemTextCenter: {
+		fontSize: '5em',
+		color: '#4CAF50',
+		display: 'flex',
+		justifyContent: 'center'
+	},
+	listItemTextRight: {
+		fontSize: '5em',
+		color: '#4CAF50',
+		display: 'flex',
+		justifyContent: 'flex-end'
+	}
+}));
 
 export const Why = () => {
-	return <Typography variant='h1'>Why</Typography>;
+	const classes = useStyles();
+
+	return (
+		<List className={classes.root} component='nav'>
+			<ListItem>
+				<ListItemText
+					classes={{ primary: classes.listItemTextLeft }}
+					primary='Fighting regressions'
+				/>
+			</ListItem>
+			<Divider />
+			<ListItem>
+				<ListItemText
+					classes={{ primary: classes.listItemTextCenter }}
+					component='nav'
+					primary='Inbuilt tool'
+				/>
+			</ListItem>
+			<Divider />
+			<ListItem>
+				<ListItemText
+					classes={{ primary: classes.listItemTextRight }}
+					component='nav'
+					primary='Fast process'
+				/>
+			</ListItem>
+		</List>
+	);
 };
