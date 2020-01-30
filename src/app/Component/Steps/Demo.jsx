d5a47d04aebb335778f7d873d149a4f2154e6462
Le Gougaud Thomas <tom.legougaud@gmail.com>
2020-01-30 22:36:11 +0100

    lookingforme?

--- src/app/Component/Steps/Demo.jsx
+++ src/app/Component/Steps/Demo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import gif from '../../../assets/images/ok.gif';
+import gif from '../../../assets/images/umbrella.gif';
 
 export const Demo = () => {
 	return <img style={{ width: '750px' }} src={gif} alt='' />;
