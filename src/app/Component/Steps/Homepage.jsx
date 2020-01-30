15e23677f0088fe4bb40a6f5b2d0901545de6e7c
Le Gougaud Thomas <tom.legougaud@gmail.com>
2020-01-30 22:32:37 +0100

    added powered by

--- src/app/Component/Steps/Homepage.jsx
+++ src/app/Component/Steps/Homepage.jsx
@@ -1,6 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
+import git_pic from '../../../assets/images/git.png';
+import material_pic from '../../../assets/images/material.png';
+import react_pic from '../../../assets/images/react.png';
+import webpack_pic from '../../../assets/images/webpack.png';
 
 const useStyles = makeStyles({
 	container: {
@@ -30,6 +34,17 @@ export const Homepage = () => {
 				<a href='https://github.com/thomas-lg/git-bisect-demo'>
 					https://github.com/thomas-lg/git-bisect-demo
 				</a>
+			</Typography>
+			<Typography className={classes.powered} variant='body2' component='div'>
+				<Typography variant='h4' component='div'>
+					Powered by
+				</Typography>
+				<Typography variant='body2' component='div'>
+					<img style={{ width: '60px' }} src={react_pic} alt='' />
+					<img style={{ width: '50px' }} src={material_pic} alt='' />
+					<img style={{ width: '50px' }} src={webpack_pic} alt='' />
+					<img style={{ width: '50px' }} src={git_pic} alt='' />
+				</Typography>
 			</Typography>
 		</div>
 	);
