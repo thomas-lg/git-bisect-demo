02dff6bf730c306eb0ee4689a00c75167910fffd
Le Gougaud Thomas <tom.legougaud@gmail.com>
2020-01-30 18:22:19 +0100

    card container handling steps

---
+++ src/app/Component/Steps/CardContainer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import PropTypes from 'prop-types';
+
+const useStyles = makeStyles({
+	card: {
+		minWidth: 275,
+		marginBottom: '20px',
+		flex: 1,
+		display: 'flex'
+	},
+	card_content: {
+		flex: 1,
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'space-between'
+	}
+});
+
+export const CardContainer = ({ step }) => {
+	const classes = useStyles();
+
+	return (
+		<Card className={classes.card}>
+			<CardContent className={classes.card_content}>{step}</CardContent>
+		</Card>
+	);
+};
+
+CardContainer.propTypes = {
+	step: PropTypes.object
+};
--- src/app/Container/Wizard/Wizard.jsx
+++ src/app/Container/Wizard/Wizard.jsx
@@ -4,15 +4,16 @@ import { Demo } from '../../Component/Steps/Demo';
 import { How } from '../../Component/Steps/How';
 import { What } from '../../Component/Steps/What';
 import { Why } from '../../Component/Steps/Why';
+import { CardContainer } from '../../Component/Steps/CardContainer';
 
 export const Wizard = () => {
 	return (
 		<>
-			<Homepage />
-			<What />
-			<Why />
-			<How />
-			<Demo />
+			<CardContainer step={<Homepage />} />
+			<CardContainer step={<What />} />
+			<CardContainer step={<Why />} />
+			<CardContainer step={<How />} />
+			<CardContainer step={<Demo />} />
 		</>
 	);
 };
