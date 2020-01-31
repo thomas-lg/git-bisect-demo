d06512aa6563a6a929885a9031e0fe7f8fdca076
Le Gougaud Thomas <tom.legougaud@gmail.com>
2020-01-31 10:08:14 +0100

    cleaning what slide

--- src/app/Component/Steps/What.jsx
+++ src/app/Component/Steps/What.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
 import bisect_cmd from '../../../assets/images/bisect_cmd.png';
 
-export const What = () => {
-	return <img src={bisect_cmd} alt='' />;
-};
+export const What = () => <img src={bisect_cmd} alt='' />;
