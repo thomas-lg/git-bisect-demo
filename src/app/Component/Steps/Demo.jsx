8a5cdb2f8464fe282d4f3a3250d0cd33afecf5c7
Le Gougaud Thomas <tom.legougaud@gmail.com>
2020-02-01 16:07:59 +0100

    lock file

--- src/app/Component/Steps/Demo.jsx
+++ src/app/Component/Steps/Demo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import gif from '../../../assets/images/desk.gif';
+import gif from '../../../assets/images/car.gif';
 
 export const Demo = () => {
 	return <img style={{ width: '750px' }} src={gif} alt='' />;
